b1194b4fbeb165149f547bd19b889e7bef6151a2
bromne <bromne@gmail.com>
2018-04-30 09:53:36 +0900

    ドキュメントを追加しました。

--- rest/src/main/scala/jp/opap/material/model/Repository.scala
+++ rest/src/main/scala/jp/opap/material/model/Repository.scala
@@ -1,5 +1,14 @@
 package jp.opap.material.model
 
+import jp.opap.material.model.RepositoryConfig.RepositoryInfo
+
 import scala.beans.BeanProperty
 
-case class Repository(@BeanProperty id: String, @BeanProperty name: String, @BeanProperty title: String, @BeanProperty headHash: String)
+/**
+  * 取得されたリポジトリを表現する型です。
+  *
+  * @param headHash このリポジトリの HEAD のコミットハッシュ
+  */
+// TODO: name は必要か？（GitLab でしか使わない）
+case class Repository(@BeanProperty id: String, @BeanProperty name: String, @BeanProperty title: String,
+  @BeanProperty headHash: String) extends RepositoryInfo
